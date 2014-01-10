ace5f982f655e9f2142bf258f9a26c0363453992
Alfredo Giscombe <mrpnut08@gmail.com>
2014-01-10 09:46:47 -0500

    Create Main Program JFrame

---
+++ src/com/mrpnut08/imagebeader/ImageBeader.java
@@ -0,0 +1,12 @@
+package com.mrpnut08.imagebeader;
+
+import com.mrpnut08.imagebeader.gui.MainScreen;
+
+public class ImageBeader {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new MainScreen().setVisible(true);
+	}
+
+}
---
+++ src/com/mrpnut08/imagebeader/gui/MainScreen.java
@@ -0,0 +1,11 @@
+package com.mrpnut08.imagebeader.gui;
+
+import javax.swing.JFrame;
+
+public class MainScreen extends JFrame {
+	
+	public MainScreen() {
+		super("Image Beader");
+		this.setSize(800, 600);
+	}
+}
