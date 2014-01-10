0dbe37f38146f06a27d47ca38bd2a338ae06b3a4
Alfredo Giscombe <mrpnut08@gmail.com>
2014-01-10 15:24:24 -0500

    Add the capability to load an image from the file system.

--- src/com/mrpnut08/imagebeader/gui/MainScreen.java
+++ src/com/mrpnut08/imagebeader/gui/MainScreen.java
@@ -1,15 +1,22 @@
 package com.mrpnut08.imagebeader.gui;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 import com.mrpnut08.imagebeader.imaging.UnbeadedImage;
 
-public class MainScreen extends JFrame {
+public class MainScreen extends JFrame implements ActionListener{
 	
 	private JLabel unbeadedimg_holder;
 	private UnbeadedImage unbeadedimg;
@@ -21,6 +28,7 @@ public class MainScreen extends JFrame {
 		this.setSize(800, 600);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLayout(new FlowLayout(FlowLayout.CENTER));
+		this.setResizable(true);
 		
 		this.generateOptionPanel();
 		this.generateUnbeadedPreviewPanel();
@@ -31,19 +39,48 @@ public class MainScreen extends JFrame {
 		this.unbeadedimg_holder = new JLabel();
 		this.add(this.unbeadedimg_holder);
 		
-		try {
-			
-		 this.unbeadedimg.loadImage("/home/algis-kun/Downloads/s1.png");
-		 this.unbeadedimg_holder.setIcon(this.unbeadedimg.getImageIcon());
-		 this.source_imgpath.setText(unbeadedimg.getFilePath());
-		
-		} catch(IOException io_error){
-			JOptionPane.showMessageDialog(this, io_error.getMessage());
-		}
 	}
 	
 	private void generateOptionPanel() {
+		JPanel panel = new JPanel(new FlowLayout());
+		
+		JButton button = new JButton("Open File");
+		button.setActionCommand("FileOpen");
+		button.addActionListener(this);
+		
 		this.source_imgpath = new JLabel("No file selected");
-		this.add(this.source_imgpath);
+		
+		
+		panel.add(this.source_imgpath);
+		panel.add(button);
+		this.add(panel);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		switch(e.getActionCommand()) {
+			
+			case "FileOpen":
+				try {
+					
+					JFileChooser filebrowser = new JFileChooser();
+					filebrowser.setFileFilter(new FileNameExtensionFilter("Image Files",
+																		  ImageIO.getReaderFormatNames()));
+					int returnVal = filebrowser.showOpenDialog(this);
+					
+					if(returnVal == JFileChooser.APPROVE_OPTION) {
+						this.unbeadedimg.loadImage(filebrowser.getSelectedFile());
+						this.unbeadedimg_holder.setIcon(this.unbeadedimg.getImageIcon());
+						this.source_imgpath.setText(unbeadedimg.getFilePath());
+					}// if(returnVal == JFileChooser.APPROVE_OPTION)
+					
+				} catch(IOException io_error){
+					JOptionPane.showMessageDialog(this, io_error.getMessage());
+				} //try
+			break;
+			
+		} //switch(e.getActionCommand())
+		
 	}
 }
