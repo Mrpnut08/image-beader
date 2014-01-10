51dad05828d294a622ad850928ea2f5a79b81795
Alfredo Giscombe <mrpnut08@gmail.com>
2014-01-10 10:52:54 -0500

    Add a icon view to the main screen to preview the loaded image.

--- src/com/mrpnut08/imagebeader/gui/MainScreen.java
+++ src/com/mrpnut08/imagebeader/gui/MainScreen.java
@@ -1,11 +1,35 @@
 package com.mrpnut08.imagebeader.gui;
 
+import java.io.IOException;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import com.mrpnut08.imagebeader.imaging.UnbeadedImage;
 
 public class MainScreen extends JFrame {
+	
+	private JLabel unbeadedimg_holder;
+	private UnbeadedImage unbeadedimg;
 	
 	public MainScreen() {
 		super("Image Beader");
 		this.setSize(800, 600);
+		
+		this.generateUnbeadedPreviewPanel();
+	}
+	
+	private void generateUnbeadedPreviewPanel() {
+		this.unbeadedimg_holder = new JLabel();
+		this.add(this.unbeadedimg_holder);
+		
+		try {
+			
+		 this.unbeadedimg = new UnbeadedImage("/home/algis-kun/Downloads/s1.png");
+		 this.unbeadedimg_holder.setIcon(this.unbeadedimg.getImageIcon());
+		
+		} catch(IOException io_error){
+			
+		}
 	}
 }
