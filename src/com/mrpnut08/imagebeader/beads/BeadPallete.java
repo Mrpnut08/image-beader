8198b03e2de076092fe52d990c2aa6cf1c454e53
Alfredo Giscombe <mrpnut08@gmail.com>
2014-01-15 14:37:48 -0500

    Create BeadPallete class which hold available beads colors and can find bead substitude colors.

---
+++ src/com/mrpnut08/imagebeader/beads/BeadPallete.java
@@ -0,0 +1,9 @@
+package com.mrpnut08.imagebeader.beads;
+
+import java.util.Set;
+import com.mrpnut08.imagebeader.beads.BeadColor;
+
+public class BeadPallete {
+	
+	private Set<BeadColor> pallete_colors;
+}
