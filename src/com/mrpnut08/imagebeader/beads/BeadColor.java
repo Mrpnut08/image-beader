f5336948d32204c13f8036afbd4061ce72482f19
Alfredo Giscombe <mrpnut08@gmail.com>
2014-01-15 14:36:29 -0500

    Create BeadColor class which handles a color that a bit can be.

---
+++ src/com/mrpnut08/imagebeader/beads/BeadColor.java
@@ -0,0 +1,29 @@
+package com.mrpnut08.imagebeader.beads;
+
+import java.awt.Color;
+
+public class BeadColor {
+	private String name,
+				   id;
+	private Color color;
+	
+	
+	public BeadColor(String id, String name, int red, int green, int blue, int alpha) {
+		this.id = id;
+		this.name = name;
+		
+		this.color = new Color(red, green, blue, alpha);
+	}
+	
+	public Color getColor() {
+		return this.color;
+	}
+	
+	public String getId() {
+		return this.id;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+}
