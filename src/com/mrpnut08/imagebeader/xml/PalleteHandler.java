7ff5df4eac579b246fbe28d4de69540d30e2e17b
Alfredo Giscombe <mrpnut08@gmail.com>
2014-01-16 20:17:04 -0500

    Create Palllete Handler class that reads the bead pallete XML files and is used by the BeadPallete class to feed itself with data.

---
+++ src/com/mrpnut08/imagebeader/xml/PalleteHandler.java
@@ -0,0 +1,33 @@
+package com.mrpnut08.imagebeader.xml;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+
+public class PalleteHandler extends XMLHandler{
+	
+	public PalleteHandler(String filename) throws Exception {
+		super(filename);
+	}
+
+	@Override
+	public void startElement(String uri, String localName, String qName,
+			Attributes attributes) throws SAXException {
+		// TODO Auto-generated method stub
+		super.startElement(uri, localName, qName, attributes);
+	}
+	
+	@Override
+	public void characters(char[] ch, int start, int length)
+			throws SAXException {
+		// TODO Auto-generated method stub
+		super.characters(ch, start, length);
+	}
+	
+	@Override
+	public void endElement(String uri, String localName, String qName)
+			throws SAXException {
+		// TODO Auto-generated method stub
+		super.endElement(uri, localName, qName);
+	}
+
+}
