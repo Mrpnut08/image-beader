f112453755820f06e7626948ca031d05f378fdae
Alfredo Giscombe <mrpnut08@gmail.com>
2014-01-18 23:12:10 -0500

    Add file reading constructor to parse and load the class using xml pallete files

--- src/com/mrpnut08/imagebeader/beads/BeadPallete.java
+++ src/com/mrpnut08/imagebeader/beads/BeadPallete.java
@@ -1,15 +1,23 @@
 package com.mrpnut08.imagebeader.beads;
 
-import java.util.Set;
-import com.mrpnut08.imagebeader.beads.BeadColor;
+import java.io.File;
+import java.util.HashSet;
+
+import com.mrpnut08.imagebeader.xml.PalleteHandler;
 
 public class BeadPallete {
 	
 	private String name;
-	private Set<BeadColor> pallete_colors;
+	private HashSet<BeadColor> pallete_colors;
 	
 	public BeadPallete (String name) {
 		this.name = name;
+	}
+	
+	public BeadPallete (File file) throws Exception{
+		PalleteHandler xmlreader = new PalleteHandler(file);
+		this.name = xmlreader.getPalleteName();
+		this.pallete_colors = xmlreader.getBeadColorSet();
 	}
 	
 	public String getName(){
