e8211342aa18ad8fbacb20082bc57af4293019b3
Alfredo Giscombe <mrpnut08@gmail.com>
2014-02-13 09:47:23 -0500

    Add pallete so that it is loaded into the program when it first starts. Refactor GUI so that it runs in private methods.

--- src/com/mrpnut08/imagebeader/gui/MainScreen.java
+++ src/com/mrpnut08/imagebeader/gui/MainScreen.java
@@ -18,82 +18,100 @@ import javax.swing.JTabbedPane;
 import javax.swing.SwingConstants;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
+import com.mrpnut08.imagebeader.beads.BeadPallete;
 import com.mrpnut08.imagebeader.imaging.UnbeadedImage;
 
-public class MainScreen extends JFrame implements ActionListener{
+public class MainScreen extends JFrame implements ActionListener {
+
+	private static final long serialVersionUID = 1L;
 
 	private JTabbedPane image_tab_holder;
 	private JLabel unbeaded_image_holder;
 	private UnbeadedImage unbeaded_image;
+	
+	private BeadPallete pallete;
 
 	private JLabel source_imgpath;
-	
+
 	public MainScreen() {
 		super("Image Beader");
 		this.setSize(640, 480);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(true);
-		
-		JSplitPane root_content_pane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
-													  this.generateOptionPanel(), 
-													  this.generatePreviewPanel());
-		this.add(root_content_pane);
-	}
-	
-	private JTabbedPane generatePreviewPanel() {
-		this.image_tab_holder = new JTabbedPane(JTabbedPane.TOP);
-		
-		this.unbeaded_image = new UnbeadedImage();
-		this.unbeaded_image_holder = new JLabel();
-		this.unbeaded_image_holder.setMinimumSize(new Dimension(440, 480));
-		this.unbeaded_image_holder.setHorizontalAlignment(SwingConstants.CENTER);
-		this.unbeaded_image_holder.setVerticalAlignment(SwingConstants.CENTER);
-		
-		this.image_tab_holder.addTab("Source Image", this.unbeaded_image_holder);
-		return (this.image_tab_holder);
-		
-		
-	}
-	
-	private JPanel generateOptionPanel() {
-		JPanel panel = new JPanel(new FlowLayout());
-		panel.setMinimumSize(new Dimension(200, 480));
-		
-		JButton button = new JButton("Open File");
-		button.setActionCommand("FileOpen");
-		button.addActionListener(this);
-		
-		this.source_imgpath = new JLabel("No file selected");
-		
-		
-		panel.add(this.source_imgpath);
-		panel.add(button);
-		return(panel);
+
+		try {
+			this.pallete = new BeadPallete("/home/algis-kun/code/perlercolorsorter/perler_pallete.xml");
+		} catch (Exception io_error) {
+			JOptionPane.showMessageDialog(this, io_error.getMessage());
+		} // ~try
+
+		// Generate the GUI interface content.
+		this.generateGUIContent();
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
-		switch(e.getActionCommand()) {
-			
-			case "FileOpen":
-				try {
-					
-					JFileChooser filebrowser = new JFileChooser();
-					filebrowser.setFileFilter(new FileNameExtensionFilter("Image Files",
-																		  ImageIO.getReaderFormatNames()));
-					int returnVal = filebrowser.showOpenDialog(this);
-					
-					if(returnVal == JFileChooser.APPROVE_OPTION) {
-						this.unbeaded_image.loadImage(filebrowser.getSelectedFile());
-						this.unbeaded_image_holder.setIcon(this.unbeaded_image.getImageIcon());
-						this.source_imgpath.setText(unbeaded_image.getFilePath());
-					}// if(returnVal == JFileChooser.APPROVE_OPTION)
-					
-				} catch(IOException io_error){
-					JOptionPane.showMessageDialog(this, io_error.getMessage());
-				} //try
+
+		switch (e.getActionCommand()) {
+
+		case "FileOpen":
+			try {
+
+				JFileChooser filebrowser = new JFileChooser();
+				filebrowser.setFileFilter(new FileNameExtensionFilter(
+						"Image Files", ImageIO.getReaderFormatNames()));
+				int returnVal = filebrowser.showOpenDialog(this);
+
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+					this.unbeaded_image
+							.loadImage(filebrowser.getSelectedFile());
+					this.unbeaded_image_holder.setIcon(this.unbeaded_image
+							.getImageIcon());
+					this.source_imgpath.setText(unbeaded_image.getFilePath());
+				}// ~if(returnVal == JFileChooser.APPROVE_OPTION)
+
+			} catch (IOException io_error) {
+				JOptionPane.showMessageDialog(this, io_error.getMessage());
+			} // ~try
 			break;
-		} //switch(e.getActionCommand())
+		} // ~switch(e.getActionCommand())
+	}
+
+	private void generateGUIContent() {
+		JSplitPane root_content_pane = new JSplitPane(
+				JSplitPane.HORIZONTAL_SPLIT, this.generateOptionPanel(),
+				this.generatePreviewPanel());
+		this.add(root_content_pane);
+	}
+
+	private JPanel generateOptionPanel() {
+		JPanel panel = new JPanel(new FlowLayout());
+		panel.setMinimumSize(new Dimension(200, 480));
+
+		JButton button = new JButton("Open File");
+		button.setActionCommand("FileOpen");
+		button.addActionListener(this);
+
+		this.source_imgpath = new JLabel("No file selected");
+
+		panel.add(this.source_imgpath);
+		panel.add(button);
+		return (panel);
+	}
+
+	private JTabbedPane generatePreviewPanel() {
+		this.image_tab_holder = new JTabbedPane(JTabbedPane.TOP);
+
+		this.unbeaded_image = new UnbeadedImage();
+		this.unbeaded_image_holder = new JLabel();
+		this.unbeaded_image_holder.setMinimumSize(new Dimension(440, 480));
+		this.unbeaded_image_holder
+				.setHorizontalAlignment(SwingConstants.CENTER);
+		this.unbeaded_image_holder.setVerticalAlignment(SwingConstants.CENTER);
+
+		this.image_tab_holder
+				.addTab("Source Image", this.unbeaded_image_holder);
+		return (this.image_tab_holder);
+
 	}
 }
