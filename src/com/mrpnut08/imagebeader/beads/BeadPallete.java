eb57dd8b2ce1a69d5db17e9b2445e586291c424d
Alfredo Giscombe <mrpnut08@gmail.com>
2014-02-14 01:07:40 -0500

    Add Bead color matching to the beading process

--- src/com/mrpnut08/imagebeader/beads/BeadPallete.java
+++ src/com/mrpnut08/imagebeader/beads/BeadPallete.java
@@ -1,7 +1,9 @@
 package com.mrpnut08.imagebeader.beads;
 
+import java.awt.Color;
 import java.io.File;
 import java.util.HashSet;
+import java.util.Iterator;
 
 import com.mrpnut08.imagebeader.xml.PalleteHandler;
 
@@ -22,5 +24,39 @@ public class BeadPallete {
 	
 	public String getName(){
 		return this.name;
+	}
+	
+	public BeadColor findColorEquivalent(Color sample) {
+		double distance;
+		double minimal = Double.MAX_VALUE;
+		BeadColor color, current;
+		Iterator<BeadColor> iterator = this.pallete_colors.iterator();
+		
+		color = BeadColor.CLEAR;
+		
+		if(sample.getAlpha() < 191) {
+			return color;
+		}
+		
+		while (iterator.hasNext()) {
+			current = iterator.next();
+			distance = this.findDistance(sample, current.getColor());
+			if (minimal > distance) {
+			 minimal = distance;
+			 color = current;
+		 }
+		}
+	
+		return color;
+	}
+	
+	private double findDistance (Color color1, Color color2) {
+		double difference;
+		difference = Math.pow(color1.getRed() - color2.getRed(), 2)
+		 		   + Math.pow(color1.getGreen() - color2.getGreen(), 2)
+		 		   + Math.pow(color1.getBlue() - color2.getBlue(), 2);
+		difference = Math.sqrt(difference);
+		difference = Math.floor(difference);
+		return difference;
 	}
 }
--- src/com/mrpnut08/imagebeader/imaging/BeadedImage.java
+++ src/com/mrpnut08/imagebeader/imaging/BeadedImage.java
@@ -3,9 +3,12 @@ package com.mrpnut08.imagebeader.imaging;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.util.HashSet;
+import java.util.Iterator;
 
 import javax.swing.ImageIcon;
 
+import com.mrpnut08.imagebeader.beads.BeadColor;
 import com.mrpnut08.imagebeader.beads.BeadPallete;
 
 public class BeadedImage {
@@ -13,9 +16,15 @@ public class BeadedImage {
 	private final int SQUARE_SIZE = 32;
 	
 	private BufferedImage image;
+	private HashSet<String> colors_used;
+	
+	public BeadedImage() {
+		this.colors_used = new HashSet<String>();
+	}
 	
 	public void generateBeadImage (UnbeadedImage source, BeadPallete pallete) {
 		Graphics2D canvas;
+		BeadColor bead_color;
 		
 		this.image = new BufferedImage( source.getWidth()*this.SQUARE_SIZE,
 										source.getHeight()*this.SQUARE_SIZE,
@@ -23,16 +32,27 @@ public class BeadedImage {
 		canvas = this.image.createGraphics();
 		for (int y = 0; y < source.getHeight(); y++) {
 			for(int x = 0; x < source.getWidth(); x++) {
-				canvas.setColor(source.getPixel(x, y));
+				bead_color = pallete.findColorEquivalent(source.getPixel(x, y));
+				canvas.setColor(bead_color.getColor());
 				canvas.fillRect(x*this.SQUARE_SIZE, y*this.SQUARE_SIZE, this.SQUARE_SIZE, this.SQUARE_SIZE);
 				canvas.setColor(Color.WHITE);
 				canvas.drawRect(x*this.SQUARE_SIZE, y*this.SQUARE_SIZE, this.SQUARE_SIZE, this.SQUARE_SIZE);
+				canvas.drawString(bead_color.getId(), x*this.SQUARE_SIZE, (y+1)*this.SQUARE_SIZE);
+				this.colors_used.add(bead_color.getName());
 			}
 		}
 		this.image.flush();
+		this.printColorList();
 	}
 	
 	public ImageIcon getImageIcon() {
 		return (new ImageIcon(this.image));
+	}
+	
+	private void printColorList() {
+		Iterator<String> iterator = this.colors_used.iterator();
+		while (iterator.hasNext()){
+			System.out.println(iterator.next());
+		}
 	}
 }
