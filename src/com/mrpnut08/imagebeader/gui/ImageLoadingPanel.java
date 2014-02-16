84d04d854bd68f5e65db5294d1ec60869fa8ced2
Alfredo Giscombe <mrpnut08@gmail.com>
2014-02-16 14:06:25 -0500

    Start refractoring GUI into separate simpler elements and corrected bug that happens when the generate button is pressed.

---
+++ src/com/mrpnut08/imagebeader/gui/ImageLoadingPanel.java
@@ -0,0 +1,67 @@
+package com.mrpnut08.imagebeader.gui;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+/**
+ * Creates a simple panel where
+ * 
+ * @author Alfredo Giscombe (Mrpnut08)
+ * 
+ */
+public class ImageLoadingPanel extends JPanel implements ActionListener {
+
+	private static final long serialVersionUID = 1L;
+
+	private JLabel image_path;
+
+	/**
+	 * Creates the Panel and its content.
+	 */
+	public ImageLoadingPanel() {
+		//Instantiate the JPanel super-class.
+		super();
+		
+		//Create the file path label
+		this.image_path = new JLabel ("No File Selected");
+		this.add(this.image_path, BorderLayout.LINE_START);
+		
+		//Create the open file button.
+		JButton open_file_button = new JButton("Open File");
+		open_file_button.setActionCommand("FileOpen");
+		open_file_button.addActionListener(this);
+		this.add(open_file_button,BorderLayout.LINE_END);
+	 }
+
+	/**
+	 * Action for open_file_button. Displays the file chooser for the user to
+	 * select a file and notifies that a new image has been loaded.
+	 */
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		//Create file chooser window.
+		JFileChooser filebrowser = new JFileChooser();
+		
+		//Set file filter so that the file chooser only sees compatible files.
+		filebrowser.setFileFilter(new FileNameExtensionFilter("Image Files",
+				ImageIO.getReaderFormatNames()));
+		
+		//Show the file chooser to the user and store the result.
+		int returnVal = filebrowser.showOpenDialog(this);
+
+		//If the user chose a file. Display the file path and 
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			this.image_path.setText(filebrowser.getSelectedFile().getPath());
+		}
+	}
+}
--- src/com/mrpnut08/imagebeader/gui/MainScreen.java
+++ src/com/mrpnut08/imagebeader/gui/MainScreen.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionListener;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
@@ -18,6 +19,7 @@ import javax.swing.JSplitPane;
 import javax.swing.JTabbedPane;
 import javax.swing.SwingConstants;
 import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.plaf.PanelUI;
 
 import com.mrpnut08.imagebeader.beads.BeadPallete;
 import com.mrpnut08.imagebeader.imaging.BeadedImage;
@@ -30,13 +32,16 @@ public class MainScreen extends JFrame implements ActionListener {
 	private JTabbedPane image_tab_holder;
 	private JLabel unbeaded_image_holder;
 	private UnbeadedImage unbeaded_image;
-	
+
 	private BeadPallete pallete;
-	
+
 	private JLabel beaded_image_holder;
 	private BeadedImage beaded_image;
 
 	private JLabel source_imgpath;
+
+	private ImageLoadingPanel image_loader;
+	private BeadingResultPane beadingresult;
 
 	public MainScreen() {
 		super("Image Beader");
@@ -45,7 +50,8 @@ public class MainScreen extends JFrame implements ActionListener {
 		this.setResizable(true);
 
 		try {
-			this.pallete = new BeadPallete("/home/algis-kun/code/perlercolorsorter/perler_pallete.xml");
+			this.pallete = new BeadPallete(
+					"/home/algis-kun/code/perlercolorsorter/perler_pallete.xml");
 		} catch (Exception io_error) {
 			JOptionPane.showMessageDialog(this, io_error.getMessage());
 		} // ~try
@@ -56,40 +62,17 @@ public class MainScreen extends JFrame implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-
-		switch (e.getActionCommand()) {
-
-		case "FileOpen":
-			try {
-
-				JFileChooser filebrowser = new JFileChooser();
-				filebrowser.setFileFilter(new FileNameExtensionFilter(
-						"Image Files", ImageIO.getReaderFormatNames()));
-				int returnVal = filebrowser.showOpenDialog(this);
-
-				if (returnVal == JFileChooser.APPROVE_OPTION) {
-					this.unbeaded_image
-							.loadImage(filebrowser.getSelectedFile());
-					this.unbeaded_image_holder.setIcon(this.unbeaded_image
-							.getImageIcon());
-					this.source_imgpath.setText(unbeaded_image.getFilePath());
-				}// ~if(returnVal == JFileChooser.APPROVE_OPTION)
-
-			} catch (IOException io_error) {
-				JOptionPane.showMessageDialog(this, io_error.getMessage());
-			} // ~catch (IOException io_error)
-		break; // ~case "FileOpen"
+		if( this.unbeaded_image.getFilePath().isEmpty()) {
+			JOptionPane.showMessageDialog(this, "No Image has been loaded","Error", JOptionPane.ERROR_MESSAGE);
+			return;
+		}
 		
-		case "BeadImage":
-			if (this.image_tab_holder.getTabCount() < 2) {
-				this.generateBeadedImageTab();
-			}
-			
-			this.beaded_image.generateBeadImage(this.unbeaded_image, this.pallete);
-			this.beaded_image_holder.setIcon(this.beaded_image.getImageIcon());
-		break; // ~case "BeadImage"
-		
-		} // ~switch(e.getActionCommand())
+		if (this.image_tab_holder.getTabCount() < 2) {
+			this.generateBeadedImageTab();
+		}
+
+		this.beaded_image.generateBeadImage(this.unbeaded_image, this.pallete);
+		this.beaded_image_holder.setIcon(this.beaded_image.getImageIcon());
 	}
 
 	private void generateGUIContent() {
@@ -100,21 +83,20 @@ public class MainScreen extends JFrame implements ActionListener {
 	}
 
 	private JPanel generateOptionPanel() {
-		JPanel panel = new JPanel(new FlowLayout());
+		// Create main panel.
+		JPanel panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 		panel.setMinimumSize(new Dimension(200, 480));
 
-		JButton button = new JButton("Open File");
-		button.setActionCommand("FileOpen");
-		button.addActionListener(this);
+		// Create ImageLoadingPanel.
+		this.image_loader = new ImageLoadingPanel();
+		panel.add(this.image_loader);
 
-		this.source_imgpath = new JLabel("No file selected");
-		
 		JButton beadingbutton = new JButton("Generate Bead Pattern");
+		beadingbutton.setAlignmentX(CENTER_ALIGNMENT);
 		beadingbutton.setActionCommand("BeadImage");
 		beadingbutton.addActionListener(this);
-		
-		panel.add(this.source_imgpath);
-		panel.add(button);
+
 		panel.add(beadingbutton);
 		return (panel);
 	}
@@ -130,23 +112,21 @@ public class MainScreen extends JFrame implements ActionListener {
 		this.unbeaded_image_holder.setVerticalAlignment(SwingConstants.CENTER);
 
 		JScrollPane scrollpane = new JScrollPane(this.unbeaded_image_holder);
-		
-		this.image_tab_holder
-				.addTab("Source Image", scrollpane);
+
+		this.image_tab_holder.addTab("Source Image", scrollpane);
 		return (this.image_tab_holder);
 
 	}
-	
+
 	private void generateBeadedImageTab() {
 		this.beaded_image = new BeadedImage();
 		this.beaded_image_holder = new JLabel();
 		this.beaded_image_holder.setMinimumSize(new Dimension(440, 480));
-		this.beaded_image_holder
-				.setHorizontalAlignment(SwingConstants.CENTER);
+		this.beaded_image_holder.setHorizontalAlignment(SwingConstants.CENTER);
 		this.beaded_image_holder.setVerticalAlignment(SwingConstants.CENTER);
-		
+
 		JScrollPane scrollpane = new JScrollPane(this.beaded_image_holder);
-		
+
 		this.image_tab_holder.addTab("Bead Pattern", scrollpane);
 	}
 }
