414bccfc1a9316688ed0f4af5d6abe78f02e341b
Alfredo Giscombe <mrpnut08@gmail.com>
2014-02-20 18:54:56 -0500

    ImageLoadingPanel now shows a preview of the source image that was loaded (the image is scaled to fit a 200 x 200 pixel square)

--- src/com/mrpnut08/imagebeader/gui/ImageLoadingPanel.java
+++ src/com/mrpnut08/imagebeader/gui/ImageLoadingPanel.java
@@ -1,11 +1,16 @@
 package com.mrpnut08.imagebeader.gui;
 
-import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JLabel;
@@ -24,55 +29,72 @@ public class ImageLoadingPanel extends JPanel implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
 
-	private JLabel image_path;
+	private JLabel image;
 	private OnImageLoadListener listener;
 
 	/**
 	 * Creates the Panel and its content.
 	 */
 	public ImageLoadingPanel(OnImageLoadListener listener) {
-		//Instantiate the JPanel super-class.
+		// Instantiate the JPanel super-class.
 		super();
-		
-		BoxLayout boxlayout = new BoxLayout(this,BoxLayout.X_AXIS);
-		this.setLayout(boxlayout);
-		
-		//Set OnImageLoadListener
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+		// Set OnImageLoadListener
 		this.listener = listener;
-		
-		//Create the file path label
-		this.image_path = new JLabel ("No File Selected");
-		this.add(this.image_path, BorderLayout.LINE_START);
-		
-		//Create the open file button.
+
+		// Create the file path label
+		this.image = new JLabel("No File Selected");
+		this.setAlignmentX(CENTER_ALIGNMENT);
+		this.image.setVerticalTextPosition(JLabel.BOTTOM);
+		this.image.setHorizontalTextPosition(JLabel.CENTER);
+		this.add(this.image);
+
+		// Create the open file button.
 		JButton open_file_button = new JButton("Open File");
 		open_file_button.setActionCommand("FileOpen");
 		open_file_button.addActionListener(this);
-		this.add(open_file_button,BorderLayout.LINE_END);
-	 }
+		this.add(open_file_button);
+	}
 
 	/**
 	 * Action for open_file_button. Displays the file chooser for the user to
 	 * select a file and notifies that a new image has been loaded.
 	 * 
-	 * @param action not used.
+	 * @param action
+	 *            not used.
 	 */
 	@Override
 	public void actionPerformed(ActionEvent action) {
-		//Create file chooser window.
+		// Create file chooser window.
 		JFileChooser filebrowser = new JFileChooser();
-		
-		//Set file filter so that the file chooser only sees compatible files.
+
+		// Set file filter so that the file chooser only sees compatible files.
 		filebrowser.setFileFilter(new FileNameExtensionFilter("Image Files",
 				ImageIO.getReaderFormatNames()));
-		
-		//Show the file chooser to the user and store the result.
+
+		// Show the file chooser to the user and store the result.
 		int returnVal = filebrowser.showOpenDialog(this);
 
-		//If the user chose a file. Display the file path and call the listener.
+		// If the user chose a file. Display the file path and call the
+		// listener.
 		if (returnVal == JFileChooser.APPROVE_OPTION) {
-			this.image_path.setText(filebrowser.getSelectedFile().getName());
+			this.loadPreviewImage(filebrowser.getSelectedFile());
+			this.image.setText(filebrowser.getSelectedFile().getName());
 			this.listener.onImageLoad(filebrowser.getSelectedFile().getPath());
 		}
+	}
+
+	private void loadPreviewImage(File file) {
+		ImageIcon image = new ImageIcon(file.getPath());
+		
+		double scale = 200 / (double) Math.max(image.getIconHeight(), image.getIconWidth());
+		
+		image.setImage(image.getImage().getScaledInstance(
+				(int)Math.round(image.getIconWidth() * scale), 
+				(int)Math.round(image.getIconHeight() * scale),
+				Image.SCALE_FAST));
+		this.image.setIcon(image);
+
 	}
 }
