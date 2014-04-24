47edf3826ea6b7c4c797828468de1b8d8f97e08d
Alfredo Giscombe (Mrpnut08) <mrpnut08@gmail.com>
2014-04-24 18:29:01 -0500

    Added the native look and feel of the operating system to the app.

--- src/com/mrpnut08/imagebeader/ImageBeader.java
+++ src/com/mrpnut08/imagebeader/ImageBeader.java
@@ -1,12 +1,34 @@
 package com.mrpnut08.imagebeader;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import com.mrpnut08.imagebeader.gui.MainScreen;
 
 public class ImageBeader {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		new MainScreen().setVisible(true);
+		
+		try {
+			UIManager.setLookAndFeel(
+			        UIManager.getSystemLookAndFeelClassName());
+			
+			new MainScreen().setVisible(true);
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
