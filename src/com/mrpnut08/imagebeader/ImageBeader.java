e1d42687ba4b9dfbdcee0ef712968d7a729bb29b
Alfredo Giscombe (Mrpnut08) <mrpnut08@gmail.com>
2014-05-10 08:35:12 -0500

    Made code compliant with GPLv3.

--- src/com/mrpnut08/imagebeader/ImageBeader.java
+++ src/com/mrpnut08/imagebeader/ImageBeader.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2014 Alfredo Giscombe (mrpnut08) <mrpnut08@gmail.com>
+ *
+ * This file is part of image-beader.
+ *
+ * image-beader is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * image-beader is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with image-beader.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.mrpnut08.imagebeader;
 
 import javax.swing.UIManager;
@@ -5,8 +24,12 @@ import javax.swing.UnsupportedLookAndFeelException;
 
 import com.mrpnut08.imagebeader.gui.MainScreen;
 
+/** Program entry point. This class holds the main method.
+  *  
+  * @author Alfredo Giscombe (mrpnut08)
+  */
 public class ImageBeader {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
@@ -16,19 +39,10 @@ public class ImageBeader {
 			
 			new MainScreen().setVisible(true);
 			
-		} catch (ClassNotFoundException e) {
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (InstantiationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (UnsupportedLookAndFeelException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		} 
 	}
 
 }
